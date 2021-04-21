1fb16eb9453eb4ee247e90f3e31bab966476db7d
Cecilia Hortas <chortas@fi.uba.ar>
2021-04-21 18:43:56 -0300

    Add ej3

---
+++ test/test.py
@@ -0,0 +1,13 @@
+import os
+import unittest
+import subprocess
+
+class TestNetcat(unittest.TestCase):
+    def test_message_ok(self):
+        os.system("docker build -f ./Dockerfile -t \"test:latest\" .")
+        process = os.popen("docker run --network 7574-tp0_testing_net test:latest")
+        
+        message = process.readline().rstrip()
+        self.assertEqual(message, "Your Message has been received: b\'message\'")
+
+        process.close()
